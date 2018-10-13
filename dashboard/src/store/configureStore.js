a1f798006ecf0f0950db5db92c1946f6b5f5ff05
thinker3197 <ashishgupta.3197@gmail.com>
2018-10-13 15:04:32 +0530

    Setup base project

---
+++ dashboard/src/store/configureStore.js
@@ -0,0 +1,8 @@
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+
+import rootReducer from "../reducers";
+
+export default function configureStore() {
+  return createStore(rootReducer, applyMiddleware(thunk));
+}
