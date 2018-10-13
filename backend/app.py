77a1f45288ebc9121a50cfc57c51e3e11727785d
arsh23 <programmer.arsh@gmail.com>
2018-10-13 15:56:06 +0530

    added /auth & /signout

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+from flask_login import login_required
+from werkzeug.exceptions import HTTPException
 
 from db import db, login_manager
 from auth import auth
@@ -14,6 +16,18 @@ app.register_blueprint(auth)
 @app.route('/', methods=['GET'])
 def home():
     return jsonify(**{})
+
+
+@app.route('/test', methods=['GET'])
+@login_required
+def test():
+    return jsonify(**{})
+
+
+@app.errorhandler(Exception)
+def handle_error(e):
+    c = 500 if not isinstance(e, HTTPException) else e.code
+    return jsonify(**{'successful': False, 'status': c, 'error': str(e)}), c
 
 
 if __name__ == '__main__':
--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,6 @@
 import json
 from flask import Blueprint, jsonify, request
+from flask_login import login_user, logout_user
 
 from db import User
 
@@ -7,20 +8,37 @@ from db import User
 auth = Blueprint('auth', __name__, template_folder='templates')
 
 
-@auth.route('/auth', methods=['GET'])
+@auth.route('/auth', methods=['POST'])
 def _auth():
-    return jsonify(**{})
+    params = json.loads(request.get_data())
+    user = User.objects(username=params['username']).first()
+    if not user:
+        raise Exception(f'username: \'{params["username"]}\' does not exist')
+    if not user.verify_password(params['password']):
+        raise Exception(f'wrong password')
+    login_user(user)
+    user_json = {'username': user.username, 'brand': user.brand}
+    return jsonify(**{'successful': True, 'user': user_json})
 
 
 @auth.route('/register', methods=['POST'])
 def register():
-    try:
-        params = json.loads(request.get_data())
-        user = User(params['username'], params['eth'])
-        user.brand = True if params['brand'] == 'true' else False
-        user.password = params['password']
-        user.save()
-        user_json = {'username': user.username, 'brand': user.brand}
-        return jsonify(**{'successful': True, 'user': user_json})
-    except Exception as e:
-        return jsonify(**{'successful': False, 'error': str(e)})
+    params = json.loads(request.get_data())
+    if User.objects(username=params['username']).first():
+        raise Exception(f'username: \'{params["username"]}\' already exists')
+    user = User(params['username'], params['eth'])
+    user.brand = True if params['brand'] == 'true' else False
+    user.password = params['password']
+    user.save()
+    user_json = {'username': user.username, 'brand': user.brand}
+    return jsonify(**{'successful': True, 'user': user_json})
+
+
+@auth.route('/signout', methods=['POST'])
+def signout():
+    params = json.loads(request.get_data())
+    user = User.objects(username=params['username']).first()
+    if not user:
+        raise Exception(f'username: \'{params["username"]}\' does not exist')
+    logout_user()
+    return jsonify(**{'successful': True})
