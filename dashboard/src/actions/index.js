0e49f9a7d2b193f906d23a3d368bc0f092eea7a8
thinker3197 <ashishgupta.3197@gmail.com>
2018-10-14 15:32:37 +0530

    Add

--- dashboard/src/actions/index.js
+++ dashboard/src/actions/index.js
@@ -1,5 +1,6 @@
 import store from "local-storage";
 import Web3 from "web3";
+import { message } from "antd";
 
 import API from "../services/api";
 import { WHOCARES_TOKEN_KEY, WHOCARES_PROFILE_KEY } from "../constants";
@@ -85,7 +86,27 @@ export const fetchCurrentCampaignsSuccess = (currentCampaigns) => {
   };
 }
 
-export const setWeb3=()=>{
+export const createCampaign = (data) => {
+  return dispatch => {
+    return API
+      .createCampaign(data)
+      .then(res => {
+        console.log(res);
+
+        if(res.successful) {
+          message.success("Campaign created successfully!");
+
+          dispatch(fetchAllCampaigns());
+          dispatch(fetchCurrentCampaigns());
+        }
+      })
+      .catch(err => {
+        throw err;
+      });
+  };
+}
+
+export const setWeb3 = () => {
     var web3 = undefined;
 
     if(window.web3) {
@@ -103,4 +124,49 @@ export const setWeb3=()=>{
         type:"SET_WEB3",
         payload: web3
     }
+}
+
+export const fetchRankings = (name) => {
+  return dispatch => {
+    return API
+      .getRankings(name)
+      .then(res => {
+        console.log(res);
+
+        dispatch(fetchRankingsSuccess(res.stats));
+      })
+      .catch(err => {
+        throw err;
+      });
+  };
+}
+
+export const fetchRankingsSuccess = (rankings) => {
+  return {
+    type: "FETCH_RANKINGS_SUCCESS",
+    rankings
+  };
+}
+
+export const fecthCampaignDetails = (name) => {
+  return dispatch => {
+    return API
+      .getCampaignDetails(name)
+      .then(res => {
+        console.log(res);
+
+        dispatch(fecthCampaignDetailsSuccess(res.campaign, res.user_stats ? res.user_stats[res.campaign.name] : res.campaign.stats));
+      })
+      .catch(err => {
+        throw err;
+      });
+  };
+}
+
+export const fecthCampaignDetailsSuccess = (activeCampaign, activeStats) => {
+  return {
+    type: "FETCH_CAMPAIGN_DETAILS_SUCCESS",
+    activeCampaign,
+    activeStats
+  };
 }
--- dashboard/src/components/campaigns/index.js
+++ dashboard/src/components/campaigns/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Badge, Modal, Icon } from "antd";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { capitalize } from "underscore.string";
 
 import CreateCampaignModal from "./createCampaignModal";
-import { fetchAllCampaigns, fetchCurrentCampaigns } from "../../actions";
-import { GRADIENTS } from "../../constants";
+import { fetchAllCampaigns, fetchCurrentCampaigns, createCampaign } from "../../actions";
+import { GRADIENTS, LOCAL_SERVER_IP } from "../../constants";
 import adcam from "../../utils/contract";
+import { copyToClipboard } from "../../utils";
 import marketing from "../../assets/marketing.jpg";
 import "./styles.less";
 
@@ -14,6 +16,7 @@ class Campaigns extends Component {
   state = {
     visible: false,
     name: undefined,
+    description: undefined,
     type: undefined,
     constraints: undefined,
     url: undefined,
@@ -33,7 +36,7 @@ class Campaigns extends Component {
       this.setState({
         activeCampaigns: nextProps.currentCampaigns
           .map((campaign, index) => {
-            campaign.gradient = GRADIENTS[index];
+            campaign.gradient = GRADIENTS[index % GRADIENTS.length];
 
             return campaign;
           })
@@ -51,12 +54,16 @@ class Campaigns extends Component {
   };
 
   handleClickJoin = (campaign) => {
-    Modal.confirm({
-      title: "Sure you want to join this campaign?",
-      content: `${capitalize(campaign.name)} created by ${campaign.owner}`,
-      okText: "Yes",
-      cancelText: "No"
-    });
+    const { user } = this.props;
+
+    if(!user.brand && campaign.active) {
+      Modal.confirm({
+        title: "Sure you want to join this campaign?",
+        content: `${capitalize(campaign.name)} created by ${campaign.owner}`,
+        okText: "Yes",
+        cancelText: "No"
+      });
+    }
   };
 
   handleClickCreate = (e) => {
@@ -65,16 +72,31 @@ class Campaigns extends Component {
     });
   };
 
-  onOk = async(e) => {
-    const {web3} = this.props.web3;
-    const accounts=await web3.eth.getAccounts();
-    adcam.methods.fundCampaign(this.state.name).send({
-       from:accounts[0],
-       value:web3.utils.toWei(""+this.state.reserve,"ether")
-    }).on("receipt",(receipt)=>{
-         console.log(receipt);
-    })
+  onOk = async (e) => {
+    // const {web3} = this.props.web3;
+    // const accounts=await web3.eth.getAccounts();
+    // adcam.methods.fundCampaign(this.state.name).send({
+    //    from:accounts[0],
+    //    value:web3.utils.toWei(""+this.state.reserve,"ether")
+    // }).on("receipt",(receipt)=>{
+    //      console.log(receipt);
+    // })
 
+    const { name, description, type, constraints, url, reserve } = this.state;
+    const { createCampaign } = this.props;
+
+    createCampaign({
+      name,
+      desc: description,
+      type,
+      constraints: {
+        clicks: constraints
+      },
+      redirect_url: url,
+      reserve
+    });
+
+    this.onCancel();
   };
 
   onCancel = (e) => {
@@ -89,7 +111,7 @@ class Campaigns extends Component {
   };
 
   render() {
-    const { activeCampaigns, visible, name, type, constraints, url, reserve } = this.state;
+    const { activeCampaigns, visible, name, description, type, constraints, url, reserve } = this.state;
     const { campaigns, user } = this.props;
 
     return (
@@ -98,25 +120,43 @@ class Campaigns extends Component {
           <p>Active campaigns</p>
           <ul className="campaigns__active-list">
             {
-              activeCampaigns && activeCampaigns.map((ac, index) => (
-                <li key={`campaign-active-key-${index}`} style={{ background: `url(${marketing})`, backgroundSize: "cover"}}>
-                  <img style={{ background: `linear-gradient(${ac.gradient.a}, ${ac.gradient.b})`}} />
-                  <div className="campaigns__stats">
-                    <span className="stats-tag">
-                      <Badge status="success" />
-                      <p>{ ac.users ? ac.users.length : 0 }</p>
-                    </span>
-                    <span className="stats-tag">
-                      <p>{ (ac.stats.clicks / ac.constraints.clicks) * 100 }% complete</p>
-                    </span>
-                  </div>
-                  <div className="campaigns__meta">
-                    <h3>{capitalize(ac.name)}</h3>
-                    <p>Hacktoberfest is a month-long celebration of open source</p>
-                  </div>
-                  <a>More details</a>
-                </li>
-              ))
+              activeCampaigns && activeCampaigns.map((ac, index) =>{
+                let referral;
+
+                if(!user.brand) {
+                  referral = `${LOCAL_SERVER_IP.slice(7)}${ac.users.filter(u => u.username === user.username)[0].url}`;
+                }
+
+                return (
+                  <li key={`campaign-active-key-${index}`}>
+                    <div style={{ background: `url(${marketing})`, backgroundSize: "cover"}} className="campaigns__active-item">
+                      <img style={{ background: `linear-gradient(${ac.gradient.a}, ${ac.gradient.b})`}} />
+                      <div className="campaigns__stats">
+                        <span className="stats-tag">
+                          <Badge status="success" />
+                          <p>{ ac.users ? ac.users.length : 0 }</p>
+                        </span>
+                        <span className="stats-tag">
+                          <p>{ ((ac.stats.clicks / ac.constraints.clicks) * 100).toFixed(1) }% complete</p>
+                        </span>
+                      </div>
+                      <div className="campaigns__meta">
+                        <h3>{capitalize(ac.name)}</h3>
+                        <p>{capitalize(ac.desc)}</p>
+                      </div>
+                      <Link to={`/campaigns/${ac.name}`}>More details</Link>
+                    </div>
+                    {
+                      !user.brand && (
+                        <div className="campaigns__url">
+                          <a onClick={(e) => copyToClipboard(`http://${referral}`)}>{referral}</a>
+                          <Icon type="copy" />
+                        </div>
+                      )
+                    }
+                  </li>
+                );
+              })
             }
           </ul>
         </div>
@@ -140,7 +180,7 @@ class Campaigns extends Component {
                   <li key={`campaign-all-key-${index}`} style={{ background: !c.active ? "#FFE0E0" : "#C6DBF9" }} onClick={(e) => this.handleClickJoin(c)}>
                     <div className="list-data">
                       <h3>{capitalize(c.name)}</h3>
-                      <p>Hacktoberfest is a month-long celebration of open source</p>
+                      <p>{capitalize(c.desc)}</p>
                     </div>
                     <p>{capitalize(c.owner)}</p>
                     <p>{capitalize(type)}</p>
@@ -177,6 +217,7 @@ class Campaigns extends Component {
           handleOk={this.onOk}
           handleCancel={this.onCancel}
           name={name}
+          description={description}
           type={type}
           constraints={constraints}
           url={url}
@@ -201,7 +242,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     fetchAllCampaigns: () => dispatch(fetchAllCampaigns()),
     fetchCurrentCampaigns: () => dispatch(fetchCurrentCampaigns()),
-
+    createCampaign: (data) => dispatch(createCampaign(data))
   };
 }
 
